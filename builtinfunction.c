646e0016b4d7dec583a66b31656e139439c55a46
sheikhsayed37 <sheikhmohammedsayedali@gmail.com>
2025-10-31 19:35:44 +0600

    12222

---
+++ builtinfunction.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <math.h>
+
+int main()
+{
+   int value = ceil(4.4); // porer ta show korbe
+   printf("%d\n",value);
+
+int value2 = floor(4.4); // ager ta show korbe
+   printf("%d\n",value2);
+
+int value3 = round(4.4); // .5 er nicr ager ta show korbe,.5 er opore 
+                          //   .5 er opore porer ta show korbe
+   printf("%d\n",value3);
+
+   int value5 = round(4.7); 
+   printf("%d\n",value5);
+
+}
